e59b530ca756e10cc79cd73cb98949965500ad97
thisisrollie <serenchyshroland@gmail.com>
2026-02-07 12:59:05 +0100

    feat(db): add postgres compose and liquibase migrations

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,28 @@
+--liquibase formatted sql
+
+--changeset rollie:1
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    full_name  VARCHAR(128)        NOT NULL,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    role       VARCHAR(16)         NOT NULL,
+    created_at TIMESTAMPTZ         NOT NULL
+);
+
+--changeset rollie:2
+CREATE TABLE tickets
+(
+    id             BIGSERIAL PRIMARY KEY,
+    title          VARCHAR(128)              NOT NULL,
+    description    TEXT                      NOT NULL,
+    status         VARCHAR(32)               NOT NULL,
+    priority       VARCHAR(16)               NOT NULL,
+    created_by_id  INT REFERENCES users (id) NOT NULL,
+    assigned_to_id INT REFERENCES users (id),
+    created_at     TIMESTAMPTZ               NOT NULL,
+    updated_at     TIMESTAMPTZ,
+    resolved_at    TIMESTAMPTZ,
+    version        INT                       NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_tickets_status ON tickets (status);
---
+++ src/test/java/com/rolliedev/ticketflow/integration/SmokeIT.java
@@ -0,0 +1,46 @@
+package com.rolliedev.ticketflow.integration;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class SmokeIT {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "users", "tickets"
+    })
+    void checkIfTablesExist(String tableName) {
+        Integer countResult = jdbcTemplate.queryForObject("SELECT count(*) FROM information_schema.tables " +
+                                                          "WHERE table_schema = 'public' AND table_name = ?", Integer.class, tableName);
+        assertThat(countResult).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, rollie, db.changelog-1.0.sql",
+            "2, rollie, db.changelog-1.0.sql"
+    })
+    void checkIfChangesetsApplied(String id, String author, String filenameSuffix) {
+        Integer actualResult = jdbcTemplate.queryForObject("""
+                SELECT count(*)
+                FROM databasechangelog
+                WHERE id = ?
+                  AND author = ?
+                  AND filename LIKE ?
+                """, Integer.class, id, author, "%" + filenameSuffix);
+
+        assertThat(actualResult).isEqualTo(1);
+    }
+}
